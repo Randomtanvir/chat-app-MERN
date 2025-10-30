6f37817aff43221bbc40302ac39395432439dd73
Tanvir <tanvir.info07@gmail.com>
2025-10-31 03:28:46 +0600

    fix cntroller bug

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,3 +1,48 @@
+import { Message } from "../models/message.mode.js";
+import { User } from "../models/user.model.js";
+import { uploadImageInCloudinary } from "../lib/cloudinary.js";
+import { activeUserSocketId, io } from "../lib/socket.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loginUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: loginUserId } }).select(
+      "-password"
+    );
+
+    res.status(200).json({
+      filteredUsers,
+    });
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json({ messages });
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 export const sendMessage = async (req, res) => {
   try {
     const { text } = req.body;
