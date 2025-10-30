c5f1c320548b972c2e492124f60d47b6c5d0c9ec
Tanvir <tanvir.info07@gmail.com>
2025-10-31 02:32:52 +0600

    change back

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import authRouts from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import path from "path";
+// import path from "path";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -27,13 +27,13 @@ const port = process.env.PORT || 3001;
 app.use("/api/auth", authRouts);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(port, () => {
   console.log(`server running on port : ${port}`);
