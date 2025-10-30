5f50a583c5bc8ae894ee1cab8abbc44bbd947e3d
Tanvir <tanvir.info07@gmail.com>
2025-10-31 00:53:00 +0600

    change for deploy

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRouts from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -24,6 +26,14 @@ const port = process.env.PORT || 3001;
 //routes
 app.use("/api/auth", authRouts);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`server running on port : ${port}`);
