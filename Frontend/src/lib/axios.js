1c09b28fe07da4dc51d326f5175c2bc91a8c6b44
Tanvir <tanvir.info07@gmail.com>
2025-10-31 02:52:06 +0600

    production uri

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://chat-app-server-88sb.onrender.com/api",
+  // baseURL: "http://localhost:5001/api",
   withCredentials: true,
 });
